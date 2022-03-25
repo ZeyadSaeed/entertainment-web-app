403e39b816fc473ae9e2a9eb7c261b7b29858adc
Ziad Said <ziads3232@gmail.com>
2022-03-25 09:07:08 +0200

    fix bug

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -2,6 +2,7 @@ import Logo from "../components/common/Logo";
 import LoginForm from "../components/signin/Login";
 
 const Login = () => {
+  console.log(process.env.NODE_ENV);
   return (
     <div className="flex items-center w-screen h-screen flex-col px-6">
       <Logo classes="my-[7.2vh] md:my-[9vh]" />
