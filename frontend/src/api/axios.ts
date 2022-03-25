8a3116e2d97b7b665ccbe8b6d6255cba3b44281d
Ziad Said <ziads3232@gmail.com>
2022-03-25 09:12:34 +0200

    add edit axios base url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const token: string = localStorage.getItem("token");
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://entertainmentwebapp.herokuapp.com/api"
+      : "http://localhost:5000/api",
   headers: { "auth-token": token },
 });
